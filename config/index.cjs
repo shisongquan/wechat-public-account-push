f0b9380580ff49d0642d42edf66b74b4a80c3ea9
shisongquan <114295683+shisongquan@users.noreply.github.com>
2022-09-28 15:11:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oRAO86YRtYEJ6yO7I9ffikyupwlM',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'UWntMdruNQs1RHIuFl7CrdWp9mkavAUHbAxC98uQRyU',
+      useTemplateId: 'c-HqvFN0jBNpjYuNOO-yHzifONJbkCPOs3vsmvMdmFs',
       // 所在省份
       province: '广东',
       // 所在城市
